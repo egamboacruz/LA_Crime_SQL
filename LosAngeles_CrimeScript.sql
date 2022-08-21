026448249451bfaf6b94ec910d807dc446804c08
egamboacruz <egamboacruz0329@gmail.com>
2022-08-20 22:43:51 -0700

    update

--- LosAngeles_CrimeScript.sql
+++ LosAngeles_CrimeScript.sql
@@ -21,7 +21,8 @@ FROM LosAngeles_Crime.dbo.crime;
 
 -- Crime data by victim Sex
 -- male vs female victims
-SELECT vict_sex, COUNT(*) AS Cases
+SELECT vict_sex, COUNT(*) AS Cases,
+	COUNT(*) * 100.0 / SUM(COUNT(*)) over() as perc
 FROM LosAngeles_Crime.dbo.victim
 GROUP BY vict_sex;
 
@@ -32,7 +33,7 @@ GROUP BY vict_sex;
 -- F <- Female
 -- M <- Male
 
--- Glance through data
+-- Glance through data with H & X as vict_sex
 SELECT *
 FROM LosAngeles_Crime.dbo.victim AS victim
 JOIN LosAngeles_Crime.dbo.crime AS crime
@@ -41,14 +42,14 @@ WHERE victim.vict_sex IN ('H','X')
 ORDER BY vict_sex;
 
 -- How many victims with unknown sex have unknown age
-SELECT age_group,COUNT(*)
+SELECT age_group,COUNT(*) AS cases,
+	COUNT(*) * 100.0 /SUM(COUNT(*)) over() AS perc
 FROM LosAngeles_Crime.dbo.victim AS victim
-JOIN LosAngeles_Crime.dbo.crime AS crime
-ON victim.dr_no = crime.dr_no
 WHERE victim.vict_sex = 'X'
 GROUP BY age_group;
--- Age group is dirty 
+-- 90 percent of unkown sex also have unknown age
 
+-- Age group is dirty 
 ----------------------------------Clean Age_Group
 --Distinct age groups
 SELECT DISTINCT age_group, dev_stage
@@ -67,8 +68,18 @@ UPDATE LosAngeles_Crime.dbo.victim
 SET age_group = '4-12'
 WHERE age_group = '12-May';
 -- Run Distinct age group query above to recheck// Age_group is now cleaned.
------------------------------------ END OF -----------------------------------------------
+----------------------------------- END OF Cleaning Age_group -----------------------------------------------
 
 
 
+-- How many victims with unknown sex have unknown descent
+SELECT vict_descent,COUNT(*) AS cases,
+	COUNT(*) * 100.0 /SUM(COUNT(*)) over() AS perc
+FROM LosAngeles_Crime.dbo.victim AS victim
+WHERE vict_sex = 'X'
+GROUP BY vict_descent;
+-- 99 percent of the cases with unknown sex also have unkown descent
+
+-- this is an analysis on crime and the victims, without victim information I cannot do much in a victim crime analysis.
+-- I will not use X and H for analysis.
 
