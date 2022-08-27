3046df003c8aebd7600d8ae1fc416ebbaac3759c
egamboacruz <egamboacruz0329@gmail.com>
2022-08-27 15:50:03 -0700

    restructured queries

--- LosAngeles_CrimeScript.sql
+++ LosAngeles_CrimeScript.sql
@@ -17,9 +17,11 @@ FROM LosAngeles_Crime.dbo.crime;
 ------------------------- START OF EDA
 -- Crime data by victim Sex
 -- male vs female victims
-SELECT vict_sex, COUNT(*) AS Cases,
-	COUNT(*) * 100.0 / SUM(COUNT(*)) over() as perc
-FROM LosAngeles_Crime.dbo.victim
+SELECT
+	vict_sex, COUNT(*) AS Cases,
+		COUNT(*) * 100.0 / SUM(COUNT(*)) over() as perc
+FROM 
+	LosAngeles_Crime.dbo.victim
 GROUP BY vict_sex;
 
 -- victim sex included
@@ -31,16 +33,19 @@ GROUP BY vict_sex;
 
 -- Glance through data with H & X as vict_sex
 SELECT *
-FROM LosAngeles_Crime.dbo.victim AS victim
-JOIN LosAngeles_Crime.dbo.crime AS crime
-ON victim.dr_no = crime.dr_no
+FROM 
+	LosAngeles_Crime.dbo.victim AS victim
+		JOIN 
+			LosAngeles_Crime.dbo.crime AS crime
+				ON victim.dr_no = crime.dr_no
 WHERE victim.vict_sex IN ('H','X')
 ORDER BY vict_sex;
 
 -- How many victims with unknown sex have unknown age
 SELECT age_group,COUNT(*) AS cases,
 	COUNT(*) * 100.0 /SUM(COUNT(*)) over() AS perc
-FROM LosAngeles_Crime.dbo.victim AS victim
+FROM 
+	LosAngeles_Crime.dbo.victim AS victim
 WHERE victim.vict_sex = 'X'
 GROUP BY age_group;
 -- 90 percent of unkown sex also have unknown age
@@ -49,7 +54,8 @@ GROUP BY age_group;
 ----------------------------------Clean Age_Group
 --Distinct age groups
 SELECT DISTINCT age_group, dev_stage
-FROM LosAngeles_Crime.dbo.victim;
+FROM 
+	LosAngeles_Crime.dbo.victim;
 -- 4-Feb
 -- 12-May
 -- it is not supposed to be a date but an age range for grouping.
@@ -71,7 +77,8 @@ WHERE age_group = '12-May';
 -- How many victims with unknown sex have unknown descent
 SELECT vict_descent,COUNT(*) AS cases,
 	COUNT(*) * 100.0 /SUM(COUNT(*)) over() AS perc
-FROM LosAngeles_Crime.dbo.victim AS victim
+FROM 
+	LosAngeles_Crime.dbo.victim AS victim
 WHERE vict_sex = 'X'
 GROUP BY vict_descent;
 -- 99 percent of the cases with unknown sex also have unkown descent
@@ -80,7 +87,7 @@ GROUP BY vict_descent;
 -- I will not use X and H for analysis of crime but I can analyze the unknown observation and figure out why they are not being reported.
 
 
--- male vs female victims by year of 2020 & 2021
+-- male vs female victims by year (2020 & 2021)
 SELECT 
 	year(t.date_occ) AS year_occ,vict_sex, COUNT(*) AS cases
 FROM
@@ -89,7 +96,7 @@ FROM
 		LosAngeles_Crime.dbo.time AS t
 			ON v.dr_no = t.dr_no
 WHERE vict_sex IN ('F','M')
-GROUP BY vict_sex, year(t.date_occ)
+GROUP BY vict_sex, year(t.date_occ);
 
 -- male vs female victims by descent and year of 2020 & 2021
 SELECT 
@@ -99,9 +106,12 @@ FROM
 	JOIN
 		LosAngeles_Crime.dbo.time AS t
 			ON v.dr_no = t.dr_no
-WHERE vict_sex IN ('F','M')
-GROUP BY vict_sex, year(t.date_occ), v.vict_descent
-ORDER BY year(t.date_occ);
+WHERE 
+	vict_sex IN ('F','M') AND vict_descent NOT IN ('Unkown','NA')
+GROUP BY 
+	vict_sex, year(t.date_occ), v.vict_descent
+ORDER BY 
+	year(t.date_occ),vict_sex,cases DESC;
 	
 --female vs male victims by age_group and year of 2020 & 2021
 SELECT year(t.date_occ) AS year_occ, v.age_group, v.vict_sex, COUNT(*) AS cases
@@ -110,9 +120,13 @@ FROM
 		JOIN
 			LosAngeles_Crime.dbo.time AS t
 				ON v.dr_no = t.dr_no
-WHERE vict_sex IN ('F','M')
-GROUP BY year(t.date_occ),v.age_group,v.vict_sex
-ORDER BY year(t.date_occ),v.age_group
+WHERE 
+	vict_sex IN ('F','M') AND v.vict_descent NOT IN ('NA') AND
+		v.age_group != 'Unkown' --unknown is misspelled
+GROUP BY 
+	year(t.date_occ),v.age_group,v.vict_sex
+ORDER BY
+	year(t.date_occ),cases DESC
 
 -- victim Age group and victim descent by year of 2020 & 2021
 SELECT year(t.date_occ) AS year_occ, v.age_group, v.vict_descent, COUNT(*) AS cases
@@ -133,7 +147,7 @@ GROUP BY
 ORDER BY cases DESC
 
 
--- crime cases and year
+-- crime description and number of cases by year
 SELECT
 	year(t.date_occ) AS year_occ, c.crm_cd_desc AS crime, COUNT(*) AS cases
 FROM 
@@ -144,7 +158,7 @@ FROM
 GROUP BY year(t.date_occ), c.crm_cd_desc
 ORDER BY year(t.date_occ),cases DESC;
 
---crime cases and sex
+--crime description and number of cases by victim's sex
 SELECT
 	v.vict_sex AS victim_sex, c.crm_cd_desc AS crime, COUNT(*) AS cases
 FROM 
@@ -156,7 +170,7 @@ WHERE v.vict_sex IN ('F','M')
 GROUP BY v.vict_sex, c.crm_cd_desc
 ORDER BY cases DESC, v.vict_sex;
 
--- crime cases and age groups
+-- crime description and count of cases by age groups
 SELECT 
 	v.age_group, c.crm_cd_desc, COUNT(*) AS cases
 FROM
